e5665f5056761163ce3eaa4e97593b9b03587062
Megan <megan.ferrara1@gmail.com>
2018-02-05 02:14:58 -0500

    Update: 2/5/18
    I added something to the single quote part of the tokenizer and tried to see what it was doing by putting in systerm.out.println statements. There is a forever loop going on in the ending with single quote while loop. I will fix that. This is what I got so far.

--- src/search/ImprovedTokenizer.java
+++ src/search/ImprovedTokenizer.java
@@ -3,31 +3,30 @@ package search;
 import java.util.ArrayList;
 
 /**
- * An improved tokenizer class that uses the following tokenization rules:
- * - tokens are delimited by whitespace
- * - Single quotes at the beginning and end of words should be separate tokens
- * - Numbers should stay together.  A number can start with a `+' or a `-', 
- *   can have any number of digits, commas and periods interspersed, but must 
- *   end in a digit (note this is a more general definition that accepts things like ``192.168.1" 
- *   and other things like ``-129.,24.34").
- * - An abbreviation is any single letter followed by a period repeated 2 or more times.  
- *   In regex terms, ``(\w\.){2,}".  For example, ``I.B.M.", ``S.A.T." and ``p.m." are all 
- *   valid abbreviations, while ``Mr." or ``I.B" are not.  All abbreviations should have 
- *   the periods removed, i.e. ``I.B.M" becomes ``IBM".
- * - Finally, ``. , ? : ; " ` ( ) % $" should all be treated as separate tokens, regardless of 
- *   where they appear (as long as they don't conflict with the above rules).  So ``$10,000" 
- *   becomes two tokens ``$" and ``10,000" and ``I wondered,is this a test?" becomes 8 
- *   tokens, with the ``," and ``?" as separate tokens.
+ * An improved tokenizer class that uses the following tokenization rules: -
+ * tokens are delimited by whitespace - Single quotes at the beginning and end
+ * of words should be separate tokens - Numbers should stay together. A number
+ * can start with a `+' or a `-', can have any number of digits, commas and
+ * periods interspersed, but must end in a digit (note this is a more general
+ * definition that accepts things like ``192.168.1" and other things like
+ * ``-129.,24.34"). - An abbreviation is any single letter followed by a period
+ * repeated 2 or more times. In regex terms, ``(\w\.){2,}". For example,
+ * ``I.B.M.", ``S.A.T." and ``p.m." are all valid abbreviations, while ``Mr." or
+ * ``I.B" are not. All abbreviations should have the periods removed, i.e.
+ * ``I.B.M" becomes ``IBM". - Finally, ``. , ? : ; " ` ( ) % $" should all be
+ * treated as separate tokens, regardless of where they appear (as long as they
+ * don't conflict with the above rules). So ``$10,000" becomes two tokens ``$"
+ * and ``10,000" and ``I wondered,is this a test?" becomes 8 tokens, with the
+ * ``," and ``?" as separate tokens.
  * 
  * @author Megan and Isabelle
  * @version 1/30/18
  *
  */
-public class ImprovedTokenizer implements Tokenizer{
+public class ImprovedTokenizer implements Tokenizer {
 	// Given text, tokenize the text into individual tokens
 	// and return an ArrayList with those tokens
-	
-	
+
 	/**
 	 * Given text, tokenize the text into individual tokens and 
 	 * return and ArrayList with those tokens
@@ -49,13 +48,32 @@ public class ImprovedTokenizer implements Tokenizer{
 		//You will probably need something that will save this word after going through if statements
 		//splits the tokens above from whitespace around it
 		String[] tempTokens = text.split(" "); 
-		System.out.println(text.toString());
+		System.out.println("# of Tokens split on whitespace:"+tempTokens.length);
+		
+		
 		
 		//2: Check for single quotes at the beginning and end of words and separate from tokens
 		//You will probably pass the tempTokens through two checkers to check for at the beginning and at the end
-		if(text.startsWith("'")) {
-	
+		//ArrayList<String> singleQuotes = new ArrayList<String>;
+		
+		//Temp: tempTokens --> this is basically duplicating the tempTokens string to look through
+		for(String temp: tempTokens) {
+			while(temp.startsWith("'")) {
+				temp.split("'");
+				System.out.println("# of Starts with SingleQuote Tokens:"+tempTokens.length);
+				//singleQuotes.add("'");
+				//text = text.substring(1);
+			}
+			while(temp.endsWith("'")) {
+				temp.split("'");
+				//System.out.println("Ends with SingleQuote Tokens:"+tempTokens.length);
+				//text = text.substring(0, text.length()-1);
+			}
+				
 		}
+		
+		
+		
 		
 		//3: Numbers stay together. Can start with "+" or "-". Can have any number of digits, commas and periods. 
 		//Must end in a digit 
@@ -69,12 +87,12 @@ public class ImprovedTokenizer implements Tokenizer{
 		
 		return null;
 	}
-	
+
 	/**
-	 * This is just here to help you test some examples.
-	 * You may remove it if you want, but I encourage you to write similar tests.
+	 * This is just here to help you test some examples. You may remove it if you
+	 * want, but I encourage you to write similar tests.
 	 */
-	public void test(){
+	public void test() {
 		String test = "The N.Y.S.E. is up $10,000 or 1%.";
 		ArrayList<String> answer = new ArrayList<String>();
 		answer.add("The");
@@ -88,41 +106,43 @@ public class ImprovedTokenizer implements Tokenizer{
 		answer.add("%");
 		answer.add(".");
 		runTest(test, answer);
-	
+
 		test = "1,000,000.00";
 		answer = new ArrayList<String>();
 		answer.add(test);
 		runTest(test, answer);
-		
+
 		test = "0.1234";
 		answer = new ArrayList<String>();
 		answer.add(test);
-		runTest(test, answer);	
+		runTest(test, answer);
 	}
-	
+
 	/**
-	 * Test helper.  Tokenizes the test string and compares
-	 * the result to the answer.  Outputs pass or fail.
+	 * Test helper. Tokenizes the test string and compares the result to the answer.
+	 * Outputs pass or fail.
 	 * 
-	 * @param test test string
-	 * @param answer the correct tokenization of test
+	 * @param test
+	 *            test string
+	 * @param answer
+	 *            the correct tokenization of test
 	 */
-	private void runTest(String test, ArrayList<String> answer){
+	private void runTest(String test, ArrayList<String> answer) {
 		// tokenize the test string
 		ArrayList<String> result = tokenize(test);
-		
-		if( !checkResult(result, answer) ){
+
+		if (!checkResult(result, answer)) {
 			System.out.println("Failed: " + test);
-			
-			for( String s: result ){
+
+			for (String s : result) {
 				System.out.print(s + " ");
 				System.out.println();
 			}
-		}else{
+		} else {
 			System.out.println("Passed: " + test);
 		}
 	}
-		
+
 	/**
 	 * Compares two ArrayLists of strings to see if they're the same.
 	 * 
@@ -130,16 +150,16 @@ public class ImprovedTokenizer implements Tokenizer{
 	 * @param answer
 	 * @return true if the ArrayLists are the same, false otherwise
 	 */
-	private boolean checkResult(ArrayList<String> result, ArrayList<String> answer){
-		if( result.size() != answer.size() ){
+	private boolean checkResult(ArrayList<String> result, ArrayList<String> answer) {
+		if (result.size() != answer.size()) {
 			return false;
-		}else{
-			for( int i = 0; i < result.size(); i++ ){
-				if( !result.get(i).equals(answer.get(i)) ){
+		} else {
+			for (int i = 0; i < result.size(); i++) {
+				if (!result.get(i).equals(answer.get(i))) {
 					return false;
 				}
 			}
-			
+
 			return true;
 		}
 	}
